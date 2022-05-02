4e1543e488b42f06f88e454ce5d01623a56f509f
Danny MacDonald <danny@dannymacdonald.me>
2022-05-01 22:32:15 -0500

    text update

--- src/components/pages/registration/RegisterHero.js
+++ src/components/pages/registration/RegisterHero.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Box, Button, Heading, Text, VStack } from '@chakra-ui/react';
+import { Box, Heading, Icon, Text, VStack } from '@chakra-ui/react';
 import { heroHeader, heroStack, heroText } from '../home/heroStyles';
+import { FaAngleDown } from 'react-icons/fa';
 
 export const RegisterHero = props => {
   return (
     <Box as="section" id="hero">
       <VStack {...heroStack} spacing={12}>
-        <Heading {...heroHeader}>Join today; observe tonight.</Heading>
+        <Heading {...heroHeader}>Join today.</Heading>
         <Text {...heroText}>
           Interested in joining? Club dues are $36 per year, pro-rated your
           first year. A new membership is currently <strong>$33</strong>. Your
-          family joins with you, for free. All we require is name, email address
-          and phone number. You do not have to be a resident of the Oklahoma
-          City Metro to join our astronomy club.
+          family joins with you, for free. You do not have to be a resident of
+          the Oklahoma City Metro to join our astronomy club.
         </Text>
-        <Button colorScheme="blue" size="lg">
-          Get Started
-        </Button>
+        <Icon as={FaAngleDown} w={8} h={8} color="white" />
       </VStack>
     </Box>
   );
